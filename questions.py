e17742811cb50ded8d4575c76785867aabb76427
Iklaash <102652384+Iklaash@users.noreply.github.com>
2023-03-22 03:12:25 -0400

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,29 @@
+question_bank = [
+    {"Question": "What is the largest planet in our solar system?,",
+     "Choices": ["A. Neptune", "B. Jupiter","C. Earth", "D. Saturn"],
+     "Answer": "b"},
+
+     {"Question": "Where does a shrimp's heart reside?",
+     "Choices": ["A. Head", "B. Tail","C. Eye", "D. Chest"],
+     "Answer": "a"},
+
+     {"Question": "Where are kangaroos native to?,",
+     "Choices": ["A. Austrailia", "B. USA","C. Korea", "D. Denmark"],
+     "Answer": "a"},
+
+     {"Question": "What is known as the only flying mammal?,",
+     "Choices": ["A. BearHawk", "B. SugarGlider","C. Platypus", "D. Bat"],
+     "Answer": "d"},
+
+     {"Question": "Which country is the largest producer of chocolate?",
+     "Choices": ["A. Switzerland", "B. Belgium", "C. Germany", "D. The United States"],
+     "Answer": "b"},
+    
+    {"Question": "What is the capital of Australia?",
+     "Choices": ["A. Melbourne", "B. Sydney", "C. Canberra", "D. Brisbane"],
+     "Answer": "c"},
+    
+    {"Question": "What is the highest mountain in Africa?",
+     "Choices": ["A. Mount Everest", "B. Kilimanjaro", "C. Mount Fuji", "D. Matterhorn"],
+     "Answer": "b"}
+]
